7e64e128a8efef2ac3d6ee9286ad285fa27c516b
xabier <xabier0123@gmail.com>
2018-12-14 13:45:48 +0100

    Boton comprar y favorito añadido, pero no comprobado

--- app/src/main/java/com/example/misaki/rakutentv/fragments/FragmentoInfoPelicula.java
+++ app/src/main/java/com/example/misaki/rakutentv/fragments/FragmentoInfoPelicula.java
@@ -1,12 +1,14 @@
 package com.example.misaki.rakutentv.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +16,18 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.misaki.rakutentv.LoginActivity;
+import com.example.misaki.rakutentv.MainActivity;
 import com.example.misaki.rakutentv.R;
 import com.example.misaki.rakutentv.adaptadores.AdaptadorPeliculasRV;
+import com.example.misaki.rakutentv.beans.Cliente;
 import com.example.misaki.rakutentv.beans.Pelicula;
 import com.example.misaki.rakutentv.dataGlobal.RakutenTvData;
+import com.example.misaki.rakutentv.tools.Post;
+
+import org.json.JSONArray;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,6 +35,8 @@ import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FragmentoInfoPelicula extends Fragment{
 
@@ -41,7 +52,7 @@ public class FragmentoInfoPelicula extends Fragment{
     ImageView imageViewFoto;
     TextView txtTitulo, txtAnio, txtGenero, txtDirector, txtEstudio, txtScore, txtSinopsis;
     Switch switchFav;
-    Button btnComprar;
+    Button btnComprar, btnFavorito;
     Pelicula pelicula;
 
 
@@ -73,6 +84,7 @@ public class FragmentoInfoPelicula extends Fragment{
 
         imageViewFoto = (ImageView) vista.findViewById(R.id.idImagen);
         btnComprar = (Button) vista.findViewById(R.id.idBtnComprar);
+        btnFavorito = (Button) vista.findViewById(R.id.idBtnComprar);
 //        switchFav = (Switch) vista.findViewById(R.id.infoFav);
 
         txtAnio = (TextView) vista.findViewById(R.id.infoAnio);
@@ -93,6 +105,33 @@ public class FragmentoInfoPelicula extends Fragment{
         txtDirector.setText(pelicula.getDirector());
         txtSinopsis.setText(pelicula.getSinopsis());
         txtAnio.setText(String.valueOf(pelicula.getAnio()));
+
+        btnComprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                HashMap<String, String> parametros = new HashMap<String, String>();
+                parametros.put("ACTION", "PELICULA.COMPRAR");
+                parametros.put("inputFavoritoUsuario", String.valueOf(RakutenTvData.getCliente().getId_usuario()));
+                parametros.put("inputFavoritoPelicula", String.valueOf(pelicula.getId_pelicula()));
+
+                TareaSegundoPlano tarea = new TareaSegundoPlano(parametros);
+                tarea.execute("http://"+RakutenTvData.getMiIP()+":8080/RakutenTV/Controller");
+            }
+        });
+        btnFavorito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                HashMap<String, String> parametros = new HashMap<String, String>();
+                parametros.put("ACTION", "PELICULA.DELETE_ADD_FAVORITOS");
+                parametros.put("inputComprarUsuario", String.valueOf(RakutenTvData.getCliente().getId_usuario()));
+                parametros.put("inputComprarPelicula", String.valueOf(pelicula.getId_pelicula()));
+
+                TareaSegundoPlano2 tarea = new TareaSegundoPlano2(parametros);
+                tarea.execute("http://"+RakutenTvData.getMiIP()+":8080/RakutenTV/Controller");
+            }
+        });
         return vista;
     }
 
@@ -186,5 +225,114 @@ public class FragmentoInfoPelicula extends Fragment{
             return inputStream;
         }
     }
+
+    class TareaSegundoPlano extends AsyncTask<String, Integer, Boolean> {
+
+        //        private ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        private HashMap<String, String> parametros = null;
+        private int aniadido = -1;
+
+
+        public TareaSegundoPlano(HashMap<String, String> parametros) {
+            this.parametros = parametros;
+        }
+
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String url_select = params[0];
+            try {
+                Post post = new Post();
+
+                JSONArray result = post.getServerDataPost(parametros, url_select);
+                aniadido = Integer.parseInt(result.toString());
+            } catch (Exception e) {
+                Log.e("log_tag", "Error in http connection " + e.toString());
+                //messageUser = "Error al conectar con el servidor. ";
+            }
+            return true;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            int progreso = values[0].intValue();
+//            progressDialog.setProgress(progreso);
+        }
+
+        /*
+         * onPostExecute().
+         * Se ejecutará cuando finalice nuestra tarea, o dicho de otra forma,
+         * tras la finalización del método doInBackground().
+         * */
+        @Override
+        protected void onPostExecute(Boolean resp) {
+            try {
+                if (aniadido != -1 && aniadido == 1 ) {
+                    Toast.makeText(LoginActivity.getInstance().getBaseContext(),"Exito! Has comprado la pelicula. ", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(LoginActivity.getInstance().getBaseContext(),"Esta pelicula ya la tienes comprada. ", Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+                Log.e("log_tag", "Error parsing data " + e.toString());
+            }
+        }
+    }
+
+    class TareaSegundoPlano2 extends AsyncTask<String, Integer, Boolean> {
+
+        //        private ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+        private HashMap<String, String> parametros = null;
+        private int aniadido = -1;
+
+
+        public TareaSegundoPlano2(HashMap<String, String> parametros) {
+            this.parametros = parametros;
+        }
+
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String url_select = params[0];
+            try {
+                Post post = new Post();
+
+                JSONArray result = post.getServerDataPost(parametros, url_select);
+                aniadido = Integer.parseInt(result.toString());
+            } catch (Exception e) {
+                Log.e("log_tag", "Error in http connection " + e.toString());
+                //messageUser = "Error al conectar con el servidor. ";
+            }
+            return true;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            int progreso = values[0].intValue();
+//            progressDialog.setProgress(progreso);
+        }
+
+        /*
+         * onPostExecute().
+         * Se ejecutará cuando finalice nuestra tarea, o dicho de otra forma,
+         * tras la finalización del método doInBackground().
+         * */
+        @Override
+        protected void onPostExecute(Boolean resp) {
+            try {
+                if (aniadido != -1 && aniadido == 1 ) {
+                    Toast.makeText(LoginActivity.getInstance().getBaseContext(),"Exito! Añadido a favorito. ", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(LoginActivity.getInstance().getBaseContext(),"Exito! Eliminado de favorito. ", Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+                Log.e("log_tag", "Error parsing data " + e.toString());
+            }
+        }
+    }
+
 
 }
